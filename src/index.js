e91fea121f044f6c755823ddf4ec7c92bd461e7e
reoszo <reoszo@126.com>
2019-10-02 20:22:25 +0800

    加载css资源模块，head style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import './index.css';
 
 function component() {
   let element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  element.classList.add('hello');
 
   return element;
 }
