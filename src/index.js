e64f913f5a658f87d8d91683c10c2aebdffc283e
reoszo <reoszo@126.com>
2019-10-03 23:16:10 +0800

    prod不默认使用压缩，只是启用source-map

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import print from './print';
 import './index.css';
 
 function component() {
-  let element = document.createElement('div');
-  let btn = document.createElement('button');
+  var ele = document.createElement('div');
+  var btn = document.createElement('button');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  ele.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  ele.classList.add('hello');
   btn.innerHTML = 'Click me and check the console!';
 
-  btn.onclick = () => {
+  btn.onclick = function () {
     print();
-  }
+  };
 
-  element.appendChild(btn);
+  ele.appendChild(btn);
 
-  return element;
+  return ele;
 }
 
 document.body.appendChild(component());
 
 if (module.hot) {
-  module.hot.accept('./print.js', function() {
+  module.hot.accept('./print.js', function () {
     console.log('you can click btn and check the console, print() is already changed');
   });
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const webpackMerge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpackCommonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(webpackCommonConfig, {
   devtool: 'source-map',
-  mode: 'production',
+  mode: 'none',
+  plugins: [
+    // new UglifyJSPlugin({
+    //   // sourceMap: true,
+    // }),
+  ],
 });
