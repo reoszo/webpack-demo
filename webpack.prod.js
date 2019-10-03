5d872786c31cdec0481edc07a603e1879d0e6c74
reoszo <reoszo@126.com>
2019-10-03 23:30:18 +0800

    build，代码可运行

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = webpackMerge(webpackCommonConfig, {
       sourceMap: true,
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
 });
